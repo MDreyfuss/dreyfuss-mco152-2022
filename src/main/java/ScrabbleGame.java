9c57470fc0c8364b577093c307d7b287c4e77493
MDreyfuss <76665915+MDreyfuss@users.noreply.github.com>
2022-02-20 01:07:04 -0500

    scrabble game: refill tiles & check word

--- src/main/java/ScrabbleGame.java
+++ src/main/java/ScrabbleGame.java
@@ -1,15 +1,17 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class ScrabbleGame {
 
     List<String> playedWords = new ArrayList<>();
     List<Character> tiles = new ArrayList<>();
     ScrabbleDictionary dictionary = new ScrabbleDictionary();
+    Random randomLetter = new Random();
 
     public ScrabbleGame()
     {
-        //give the player 7 random tiles (ASCII values)
+        refillTiles();
     }
 
     /**
@@ -17,11 +19,35 @@ public class ScrabbleGame {
      * and the letters exist in the tiles list
      * remove the letters from the list and add new random tiles
      *
-     * @param word
+     * @param word: Word to be tried
      */
     public boolean playWord(String word)
     {
+        word = word.toUpperCase();
+        if (dictionary.isWord(word))
+        {
+            for (int i = 0; i < word.length(); i++)
+            {
+                Character currChar = word.charAt(i);
+                if (!tiles.remove(currChar)) //should I use a deep copy & not ruin original list??
+                {
+                    return false;
+                }
+            }
+            refillTiles();
+            playedWords.add(word);
+            return true;
+        }
         return false;
     }
 
+    public void refillTiles()
+    {
+        for (int i = tiles.size(); i < 7; i++)
+        {
+            int newLetterValue = randomLetter.nextInt(26) + 65;
+            char newLetter = (char) newLetterValue;
+            tiles.add(newLetter);
+        }
+    }
 }
