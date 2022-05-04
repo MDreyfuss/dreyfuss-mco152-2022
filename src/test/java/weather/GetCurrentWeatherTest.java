3ada31481ce6a58c647b6523a06949a17d44ce73
MDreyfuss <76665915+MDreyfuss@users.noreply.github.com>
2022-05-04 02:12:35 -0400

    fix tests

--- src/test/java/weather/GetCurrentWeatherTest.java
+++ src/test/java/weather/GetCurrentWeatherTest.java
@@ -1,6 +1,8 @@
 package weather;
 
 import io.reactivex.Observable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 import org.junit.jupiter.api.Test;
 import weather.json.CurrentWeather;
 
@@ -17,15 +19,18 @@ class GetCurrentWeatherTest {
 
         //when
         Observable<CurrentWeather> observable = getCurrentWeather.getCurrentWeather("10019");
-        CurrentWeather currentWeather = new CurrentWeather();
 
-
-        //then
+        Disposable disposable = observable
+                .subscribeOn(Schedulers.io())
+                .subscribe(this::onNext);
+    }
+    //then
+    public void onNext(CurrentWeather currentWeather)
+    {
         assertTrue(currentWeather.getTemperature() > 0);
         assertTrue(currentWeather.getMaxTemperature() > 0);
         assertTrue(currentWeather.getMinTemperature() > 0);
         assertNotNull(currentWeather.getDescription());
         assertNotNull(currentWeather.getIcon());
-
     }
 }
